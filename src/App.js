a70b1e895b44007fbdecee6adea3695566abf4b1
Teddy Abebe <teddyab729@gmail.com>
2023-07-25 16:46:58 -1100

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import BFS from './BFS/BFS';
+import React from "react";
+import "./App.css";
+import BFS from "./BFS/BFS";
 
 function App() {
   return (
@@ -10,12 +10,18 @@ function App() {
           <h1 className="title">Breadth-First Search Algorithm Visualizer</h1>
           <div className="bio">
             <p className="bio-text">
-              Hello! My name is Mark. Welcome to this interactive application designed to help you explore the Breadth-First-Search (BFS) algorithm.
+              Hello! My name is Mark. Welcome to this interactive application
+              designed to help you explore the Breadth-First-Search (BFS)
+              algorithm.
             </p>
-            <p className="bio-text">Go ahead and start clicking on different areas on the board below to see how BFS efficiently finds the shortest path between nodes.</p>
+            <p className="bio-text">
+              Go ahead and start clicking on different areas on the board below
+              to see how BFS efficiently finds the shortest path between nodes.
+            </p>
           </div>
         </header>
-      </div> {}
+      </div>{" "}
+      {}
       <BFS />
     </div>
   );
